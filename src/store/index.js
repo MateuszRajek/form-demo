66cfbed01572caf80cdd6e35aa82504de5a20df7
Mateusz Rajek <mateusz_rajek@wp.pl>
2024-02-29 18:44:15 +0100

    Added combined reducers to split dofferent state reducers

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,16 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
+import { default as formReducer } from "./Form";
 
-const initialState = {
-  form: {},
+const initialState = {};
+
+const rootPersistedReducer = combineReducers({
+  form: formReducer,
+});
+
+const rootReducer = (state, action) => {
+  return rootPersistedReducer(state, action);
 };
 
-const formReducer = (state = initialState, action) => {
-  return state;
-};
-
-const store = createStore(formReducer, initialState);
+const store = createStore(rootReducer, initialState);
 
 export default store;
