0a67b3e8a3b798ff9db3bee851e67ef2a94d0ef8
Mateusz Rajek <mateusz_rajek@wp.pl>
2024-03-01 09:22:40 +0100

    Implemented appointment details view

--- src/components/FormSteps/AppointmentDetails/AppointmentDetails.jsx
+++ src/components/FormSteps/AppointmentDetails/AppointmentDetails.jsx
@@ -1,5 +1,15 @@
-const AppointmentDetails = () => {
-  return <p>Appointment Details</p>;
+import Select from "../../formComponents/Select";
+import { appointmentType, locationData } from "../../../data/mocksData";
+import TextArea from "../../formComponents/TextArea";
+
+const AppointmentDetails = ({ register }) => {
+  return (
+    <div className="section__wrapper">
+      <Select register={register} field="Practitioner" options={appointmentType} label="Appointment type" />
+      <Select register={register} field="Location" options={locationData} label="Preffered location" />
+      <TextArea register={register} field="IssueDescription" label="Explain your skin issue" rows={10} />
+    </div>
+  );
 };
 
 export default AppointmentDetails;
