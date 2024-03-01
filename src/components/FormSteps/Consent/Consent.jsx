3335d77189d87fe120698f64ddb7b408a6560500
Mateusz Rajek <mateusz_rajek@wp.pl>
2024-03-01 14:33:56 +0100

    Implemented consent screen

--- src/components/FormSteps/Consent/Consent.jsx
+++ src/components/FormSteps/Consent/Consent.jsx
@@ -1,5 +1,23 @@
-const Consent = () => {
-  return <p>CONSENT</p>;
+import { consentOptions } from "../../../data/mocksData";
+import Checkbox from "../../formComponents/Checkbox";
+import Label from "../../formComponents/Label";
+
+const Consent = ({ register }) => {
+  return (
+    <>
+      <div className="section__wrapper">
+        <Label label="Medical insurer" required />
+        {consentOptions.map(({ consent, text }) => (
+          <Checkbox register={register} field={consent} type="checkbox" label={text} />
+        ))}
+      </div>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores fuga quos dolor eos, error temporibus unde tenetur quae! Quidem inventore adipisci neque illum quam voluptates! Dolores
+        aspernatur modi atque? Aut dolor autem qui! Porro, doloremque fugit. Fugiat, in dolor dicta quae quos sit accusamus rem corporis. Modi eligendi assumenda id mollitia hic ducimus fuga. Impedit
+      </p>
+    </>
+  );
 };
 
 export default Consent;
--- src/data/mocksData.js
+++ src/data/mocksData.js
@@ -2,5 +2,16 @@ const appointmentType = ["Dermatology", "Eye Doctor"];
 const locationData = ["Carrickmines", "Lucan"];
 const insurerCompanies = ["VHI Healthcare", "Irish Life Health", "Laya HEalthcare", "Other", "None"];
 const countries = ["Ireland", "United Kingdom", "Other"];
+const consentOptions = [
+  { consent: "AppointmentProccess", text: "Please confirm that you understand the Appointment Proccess" },
+  {
+    consent: "PrivacyPolicy",
+    text: "By submitting this form, you are agreeing to our Privacy Policy",
+  },
+  {
+    consent: "DataSharing",
+    text: "Consent to sharing your data with Vhi Healthcara",
+  },
+];
 
-export { appointmentType, locationData, insurerCompanies, countries };
+export { appointmentType, locationData, insurerCompanies, countries, consentOptions };
